be0e6a7d34f6487fef334ad1bcfa0c0c99c69d4d
Martha659 <martha.blackcloud@students.msswarriors.org>
2026-01-12 19:38:01 +0000

    coomit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+function markAsDone = [todos]
+
+
+
+
+hould create a function named markAsDone that takes an array of todos and returns the modified array
+should loop through the todos, using a while loop, and prepend the string 'done - ' to each todo description
 
